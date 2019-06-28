5c0024f2be9ed96e3ed213240e51648798907fb9
Paul Moyaert <paul.moyaert@logins.be>
2019-06-28 10:34:24 +0200

    basic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'Home',
+    component: AppComponent
+  },
+  {
+    path: '',
+    redirectTo: '/Home',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
