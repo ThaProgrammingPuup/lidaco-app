b027a2f5e49d5aa6e8c83a740fc0b18c09d6a3f1
Paul Moyaert <paul.moyaert@logins.be>
2019-10-25 14:02:02 +0200

    scroll-to-top button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { ScrollTopComponent } from './scroll-top/scroll-top.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ScrollTopComponent
   ],
   imports: [
     BrowserModule,
